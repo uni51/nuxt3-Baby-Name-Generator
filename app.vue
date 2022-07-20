24f8a50e1259ccffec7dab622892bc193d024f59
Koichi.Shimomura <unieye51@gmail.com>
2022-07-20 15:05:35 +0900

    2-17 Passing Props to the Components

--- app.vue
+++ app.vue
@@ -52,7 +52,12 @@ const optionsArray = [
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click the "Find Names" button below</p>
     <div class="options-container">
-      <Option v-for="option in optionsArray" :key="option.title" />
+      <Option
+        v-for="option in optionsArray"
+        :key="option.title"
+        :option="option"
+        :options="options"
+      />
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
     <div class="cards-container">
@@ -85,35 +90,6 @@ h1 {
   margin: 0 auto;
   margin-top: 4rem;
   position: relative;
-}
-
-.option-container {
-  margin-bottom: 2rem;
-}
-
-.option {
-  background-color: white;
-  outline: 0.15rem solid rgb(249, 87, 89);
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: rgb(27, 60, 138);
-  cursor: pointer;
-  font-weight: 200;
-}
-
-.option-left {
-  border-radius: 1rem 0 0 1rem;
-}
-
-.option-right {
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option-active {
-  background-color: rgb(249, 87, 89);
-  color: white;
 }
 
 .primary {
--- components/Option.vue
+++ components/Option.vue
@@ -1,76 +1,66 @@
+<script setup lang="ts">
+import { Gender, Popularity, Length } from "@/data";
+
+interface OptionProps {
+  option: {
+    title: string;
+    category: string;
+    button: Gender[] | Popularity[] | Length[];
+  };
+  options: {
+    gender: Gender;
+    popularity: Popularity;
+    length: Length;
+  };
+}
+
+const props = defineProps<OptionProps>();
+</script>
+
 <template>
-  <div class="options-container">
-    <div class="option-container">
-      <h4>1) Choose a gender</h4>
-      <div class="option-buttons">
-        <button
-          class="option option-left"
-          :class="options.gender === Gender.BOY && 'option-active'"
-          @click="options.gender = Gender.BOY"
-        >
-          Boy
-        </button>
-        <button
-          class="option"
-          :class="options.gender === Gender.UNISEX && 'option-active'"
-          @click="options.gender = Gender.UNISEX"
-        >
-          Unisex
-        </button>
-        <button
-          class="option option-right"
-          :class="options.gender === Gender.GIRL && 'option-active'"
-          @click="options.gender = Gender.GIRL"
-        >
-          Girl
-        </button>
-      </div>
+  <div class="option-container">
+    <h4>{{ props.option.title }}</h4>
+    <div class="option-buttons">
+      <button
+        v-for="value in option.buttons"
+        :key="value"
+        class="option option-left"
+        :class="options[option.category] === value && 'option-active'"
+        @click="options[option.category] = value"
+      >
+        {{ value }}
+      </button>
     </div>
-    <div class="option-container">
-      <h4>2) Choose the name's popularity</h4>
-      <div class="option-buttons">
-        <button
-          class="option option-left"
-          :class="options.popularity === Popularity.TRENDY && 'option-active'"
-          @click="options.popularity = Popularity.TRENDY"
-        >
-          Trendy
-        </button>
-        <button
-          class="option option-right"
-          :class="options.popularity === Popularity.UNIQUE && 'option-active'"
-          @click="options.popularity = Popularity.UNIQUE"
-        >
-          Unique
-        </button>
-      </div>
-    </div>
-    <div class="option-container">
-      <h4>3) Choose name's length</h4>
-      <div class="option-buttons">
-        <button
-          class="option option-left"
-          :class="options.length === Length.LONG && 'option-active'"
-          @click="options.length = Length.LONG"
-        >
-          Long
-        </button>
-        <button
-          class="option"
-          :class="options.length === Length.ALL && 'option-active'"
-          @click="options.length = Length.ALL"
-        >
-          All
-        </button>
-        <button
-          class="option option-right"
-          :class="options.length === Length.SHORT && 'option-active'"
-          @click="options.length = Length.SHORT"
-        >
-          Short
-        </button>
-      </div>
-    </div>
-    <button class="primary" @click="computeSelectedNames">Find Names</button>
   </div>
 </template>
+
+<style scoped>
+.option-container {
+  margin-bottom: 2rem;
+}
+
+.option {
+  background-color: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+  font-weight: 200;
+}
+
+.option-left {
+  border-radius: 1rem 0 0 1rem;
+}
+
+.option-right {
+  border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white;
+}
+</style>
